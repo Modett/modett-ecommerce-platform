74986e5e7523dcb28e33f0fc7525152ab262c87c
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-18 13:04:11 +0530

    simple updates

---
+++ modett/modules/user-management/application/queries/index.ts
@@ -0,0 +1,9 @@
+// Queries
+export * from './get-user-profile.query';
+export * from './list-addresses.query';
+
+// Base interfaces
+export {
+  IQuery,
+  IQueryHandler
+} from './get-user-profile.query';
