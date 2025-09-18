7f78e7268273af6f4feb4dd6c75939a1813637fb
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-18 14:43:43 +0530

    updated index.ts in commands

--- modett/modules/user-management/application/commands/index.ts
+++ modett/modules/user-management/application/commands/index.ts
@@ -1,7 +1,13 @@
 // Commands
 export * from './register-user.command';
+export * from './login-user.command';
 export * from './update-profile.command';
 export * from './add-address.command';
+export * from './update-address.command';
+export * from './delete-address.command';
+export * from './add-payment-method.command';
+export * from './update-payment-method.command';
+export * from './delete-payment-method.command';
 
 // Base interfaces and types
 export {
