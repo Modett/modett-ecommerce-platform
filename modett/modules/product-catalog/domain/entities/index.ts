f1e672eb3ec39046a724e4ba6d11684a402d2c26
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-23 01:01:30 +0530

    created index file for entities

---
+++ modett/modules/product-catalog/domain/entities/index.ts
@@ -0,0 +1,7 @@
+export { Product, ProductStatus, CreateProductData, ProductData, ProductRow } from './product.entity';
+export { ProductVariant, CreateVariantData, VariantData, VariantRow } from './product-variant.entity';
+export { Category, CreateCategoryData, CategoryData, CategoryRow } from './category.entity';
+export { MediaAsset, MediaAssetId, CreateMediaAssetData, MediaAssetData, MediaAssetRow } from './media-asset.entity';
+export { ProductTag, ProductTagId, CreateProductTagData, ProductTagData, ProductTagRow } from './product-tag.entity';
+export { SizeGuide, SizeGuideId, Region, CreateSizeGuideData, SizeGuideData, SizeGuideRow } from './size-guide.entity';
+export { EditorialLook, EditorialLookId, CreateEditorialLookData, EditorialLookData, EditorialLookRow } from './editorial-look.entity';
