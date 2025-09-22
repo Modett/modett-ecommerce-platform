f001cfc60c422467068784ec6583a4d4b466af93
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-22 13:20:45 +0530

    created index file in value objects

---
+++ modett/modules/product-catalog/domain/value-objects/index.ts
@@ -0,0 +1,6 @@
+export { ProductId } from './product-id.vo';
+export { VariantId } from './variant-id.vo';
+export { CategoryId } from './category-id.vo';
+export { Slug } from './slug.vo';
+export { Price } from './price.vo';
+export { SKU } from './sku.vo';
