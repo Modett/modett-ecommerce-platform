c5c6d0f7ebba5662cd5e805feb706333011de81f
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-23 21:51:15 +0530

    created index for services layer

---
+++ modett/modules/product-catalog/application/services/index.ts
@@ -0,0 +1,7 @@
+// Export all services
+export { ProductManagementService } from './product-management.service';
+export { CategoryManagementService } from './category-management.service';
+export { MediaManagementService } from './media-management.service';
+export { VariantManagementService } from './variant-management.service';
+export { ProductSearchService } from './product-search.service';
+export { SlugGeneratorService } from './slug-generator.service';
