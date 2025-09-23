1f097e87c753caaef428397cca670a17c8c46db2
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-23 15:35:31 +0530

    media management service

---
+++ modett/modules/product-catalog/application/services/media-management.service.ts
@@ -0,0 +1,9 @@
+import { IMediaAssetRepository } from '../../domain/repositories/media-asset.repository';
+
+export class MediaManagementService {
+  constructor(
+    private readonly mediaAssetRepository: IMediaAssetRepository
+  ) {}
+
+  // TODO: Implement media management methods
+}
