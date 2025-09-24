6b78da9a197f1aa29cc8914b7187b32900b25006
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-24 14:40:26 +0530

    updated index file

--- modett/modules/product-catalog/domain/repositories/index.ts
+++ modett/modules/product-catalog/domain/repositories/index.ts
@@ -5,3 +5,10 @@ export { IMediaAssetRepository, MediaAssetQueryOptions, MediaAssetCountOptions }
 export { IProductTagRepository, ProductTagQueryOptions, ProductTagCountOptions } from './product-tag.repository';
 export { ISizeGuideRepository, SizeGuideQueryOptions, SizeGuideCountOptions } from './size-guide.repository';
 export { IEditorialLookRepository, EditorialLookQueryOptions, EditorialLookCountOptions } from './editorial-look.repository';
+
+// Association repository interfaces
+export { IProductCategoryRepository, ProductCategoryQueryOptions, ProductCategoryCountOptions } from './product-category.repository';
+export { IProductMediaRepository, ProductMediaQueryOptions, ProductMediaCountOptions } from './product-media.repository';
+export { IVariantMediaRepository, VariantMediaQueryOptions, VariantMediaCountOptions } from './variant-media.repository';
+export { IProductTagAssociationRepository, ProductTagAssociationQueryOptions, ProductTagAssociationCountOptions } from './product-tag-association.repository';
+export { IEditorialLookProductRepository, EditorialLookProductQueryOptions, EditorialLookProductCountOptions } from './editorial-look-product.repository';
