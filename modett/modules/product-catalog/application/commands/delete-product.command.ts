07e1d31b56f563596529761afc7d853c59c16c1d
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-25 08:49:03 +0530

    delete product command created

--- modett/modules/product-catalog/application/commands/delete-product.command.ts
+++ modett/modules/product-catalog/application/commands/delete-product.command.ts
@@ -1,31 +1,5 @@
 import { ProductManagementService } from '../services/product-management.service';
-
-// Base interfaces
-export interface ICommand {
-  readonly commandId?: string;
-  readonly timestamp?: Date;
-}
-
-export interface ICommandHandler<TCommand extends ICommand, TResult = void> {
-  handle(command: TCommand): Promise<TResult>;
-}
-
-export class CommandResult<T = any> {
-  constructor(
-    public success: boolean,
-    public data?: T,
-    public error?: string,
-    public errors?: string[]
-  ) {}
-
-  static success<T>(data?: T): CommandResult<T> {
-    return new CommandResult(true, data);
-  }
-
-  static failure<T>(error: string, errors?: string[]): CommandResult<T> {
-    return new CommandResult<T>(false, undefined, error, errors);
-  }
-}
+import { ICommand, ICommandHandler, CommandResult } from './create-product.command';
 
 export interface DeleteProductCommand extends ICommand {
   productId: string;
