e147d86d139ded1411817a676d51eedc8b303374
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-25 08:54:31 +0530

    delete unwanted files

--- modett/modules/product-catalog/application/queries/GetProductQuery.ts
+++
@@ -1,6 +0,0 @@
-import { IQuery } from './Query';
-import { Product } from '../../domain/entities/product.entity';
-
-export interface GetProductQuery extends IQuery<Product> {
-  productId: string;
-}
