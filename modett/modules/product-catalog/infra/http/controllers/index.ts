1c4e45345e6b1e6b9653abc406c553357216a986
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-09-29 21:28:37 +0530

    updated index files

--- modett/modules/product-catalog/infra/http/controllers/index.ts
+++ modett/modules/product-catalog/infra/http/controllers/index.ts
@@ -1,6 +1,9 @@
 // Export all controllers
-export { ProductController } from './product.controller';
-export { CategoryController } from './category.controller';
-export { MediaController } from './media.controller';
-export { VariantController } from './variant.controller';
-export { SearchController } from './search.controller';
+export { ProductController } from "./product.controller";
+export { CategoryController } from "./category.controller";
+export { MediaController } from "./media.controller";
+export { VariantController } from "./variant.controller";
+export { SearchController } from "./search.controller";
+export { ProductTagController } from "./product-tag.controller";
+export { SizeGuideController } from "./size-guide.controller";
+export { EditorialLookController } from "./editorial-look.controller";
