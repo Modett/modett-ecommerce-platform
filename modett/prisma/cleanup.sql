b8c40964ec6fd8bb14b560996d5601b01abbf51d
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-12-19 18:41:02 +0530

    Removed this file

--- modett/prisma/cleanup.sql
+++
@@ -1,8 +0,0 @@
--- Cleanup script to remove existing data
-TRUNCATE TABLE "ProductMedia" CASCADE;
-TRUNCATE TABLE "ProductVariant" CASCADE;
-TRUNCATE TABLE "ProductCategory" CASCADE;
-TRUNCATE TABLE "Product" CASCADE;
-TRUNCATE TABLE "Category" CASCADE;
-TRUNCATE TABLE "MediaAsset" CASCADE;
-TRUNCATE TABLE "Location" CASCADE;
