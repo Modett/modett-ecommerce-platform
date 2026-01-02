31c5a642fa223ed892756ab85f0550a22b1f85c8
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-02 16:40:59 +0530

    Refactor analytics command exports to include shipping and payment info handlers

--- modett/modules/analytics/application/commands/index.ts
+++ modett/modules/analytics/application/commands/index.ts
@@ -3,6 +3,12 @@ export { TrackPurchaseHandler } from "./track-purchase.command";
 export { TrackAddToCartHandler } from "./track-add-to-cart.command";
 export { TrackBeginCheckoutHandler } from "./track-begin-checkout.command";
 
+// Add Shipping Info
+export * from "./track-add-shipping-info.command";
+
+// Add Payment Info
+export * from "./track-add-payment-info.command";
+
 export type { TrackProductViewCommand } from "./track-product-view.command";
 export type { TrackPurchaseCommand } from "./track-purchase.command";
 export type { TrackAddToCartCommand } from "./track-add-to-cart.command";
