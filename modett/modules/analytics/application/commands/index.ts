ab19509ad1dd75035747eac0f8417bd5d91fcbb1
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-01-02 15:05:43 +0530

    Add TrackBeginCheckout command and handler exports to analytics commands

--- modett/modules/analytics/application/commands/index.ts
+++ modett/modules/analytics/application/commands/index.ts
@@ -1,7 +1,9 @@
 export { TrackProductViewHandler } from "./track-product-view.command";
 export { TrackPurchaseHandler } from "./track-purchase.command";
 export { TrackAddToCartHandler } from "./track-add-to-cart.command";
+export { TrackBeginCheckoutHandler } from "./track-begin-checkout.command";
 
 export type { TrackProductViewCommand } from "./track-product-view.command";
 export type { TrackPurchaseCommand } from "./track-purchase.command";
 export type { TrackAddToCartCommand } from "./track-add-to-cart.command";
+export type { TrackBeginCheckoutCommand } from "./track-begin-checkout.command";
